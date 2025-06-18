bf8533a3e04fb49fb93426762fbcb87a58c2d5d8
rknm-cell <racknahm@gmail.com>
2025-06-18 08:55:19 -0400

    updated to address vercel errors

--- src/app/recipe/[id]/page.tsx
+++ src/app/recipe/[id]/page.tsx
@@ -1,16 +1,12 @@
+import React from "react";
+import RecipeDetail from "~/app/components/RecipeDetail";
+import { getRecipeById } from "~/server/db/queries";
 
-import React from 'react'
-import RecipeDetail from '~/app/components/RecipeDetail';
-import { getRecipeById } from '~/server/db/queries';
-
-export default async function Page(props: {params: {id: string} }) {
-    const { id } = props.params;
-    const recipe = await getRecipeById(id);
-
-    if (!recipe) {
-      return <div>Recipe not found</div>
-    }
-  return (
-    <RecipeDetail recipe={recipe}/>
-  )
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const { id } = await props.params;
+  const recipe = await getRecipeById(id);
+  if (!recipe) {
+    return <div>Recipe not found</div>;
+  }
+  return <RecipeDetail recipe={recipe} />;
 }
