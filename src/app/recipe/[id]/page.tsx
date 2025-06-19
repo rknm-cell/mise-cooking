7daefe360f5c5857c7f5961e15252f10a1941d01
rknm-cell <racknahm@gmail.com>
2025-06-19 14:56:55 -0400

    save point

--- src/app/recipe/[id]/page.tsx
+++ src/app/recipe/[id]/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import RecipeDetail from "~/app/components/recipes/RecipeDetail";
 import { getRecipeById } from "~/server/db/queries";
+import { api } from "~/trpc/server";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const { id } = await props.params;
-  const recipe = await getRecipeById(id);
+  console.log(id)
+  const {data: recipe} = api.recipe.getRecipe.useQuery(id);
   if (!recipe) {
     return <div>Recipe not found</div>;
   }
