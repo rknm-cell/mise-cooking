5ff6527bdf04af6f8ac2d129b1dba3281a5bd214
rknm-cell <racknahm@gmail.com>
2025-06-19 14:56:55 -0400

    save point

--- src/server/api/routers/recipe.ts
+++ src/server/api/routers/recipe.ts
@@ -1,9 +1,17 @@
+
+import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { getAllRecipes } from "~/server/db/queries";
+import { db } from "~/server/db";
+import { getAllRecipes, getRecipeById } from "~/server/db/queries";
 
 export const recipeRouter = createTRPCRouter({
   getAllRecipes: publicProcedure.query(async () => {
     const allRecipes = await getAllRecipes();
     return allRecipes;
+  }),
+  getRecipe: publicProcedure.input(z.string()).query(async (opts) => {
+    const { input } = opts;
+    const recipe = await getRecipeById(input)
+    return recipe;
   }),
 });
