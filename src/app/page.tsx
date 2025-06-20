eada9f751f258349e82654e3227821161ee375c3
rknm-cell <racknahm@gmail.com>
2025-06-20 10:40:21 -0400

    started testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import RecipeGenerator from "./components/RecipeGenerator";
+import RecipeGenerator from "./components/recipes/RecipeGenerator";
 
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#1d7b86] to-[#426b70] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          <span className="text-[#fcf45a]">Mise</span>
+          <span className="text-[#fcf45a] nanum-pen-script-regular">Mise</span>
         </h1>
 
         <RecipeGenerator />
