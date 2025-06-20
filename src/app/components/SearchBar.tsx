18ef05267837dc0de93bbe9a717e082e3de98c6f
rknm-cell <racknahm@gmail.com>
2025-06-20 10:40:21 -0400

    started testing

---
+++ src/app/components/SearchBar.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+export default function SearchBar() {
+//   const [search, setSearch] = useState("");
+
+    
+
+  return (
+    <div>
+      {/* <form onSubmit={handleSubmit} className="flex gap-2">
+        <input
+          type="text"
+          value={input}
+          onChange={handleChange}
+          placeholder="What do you want?"
+          className="flex-1 rounded-lg bg-zinc-500 px-4 py-2 text-white placeholder-white/50 focus:ring-2 focus:ring-[hsl(280,100%,70%)] focus:outline-none"
+          disabled={isLoading}
+        />
+        <button
+          type="submit"
+          disabled={isLoading || !input.trim()}
+          className="rounded-lg bg-[hsl(136,11%,33%)] px-4 py-2 font-semibold text-white hover:bg-[hsl(280,100%,60%)] focus:ring-2 focus:ring-[hsl(280,100%,70%)] focus:outline-none disabled:opacity-50 disabled:hover:bg-[hsl(280,100%,70%)]"
+        >
+          Send
+        </button>
+      </form> */}
+    </div>
+  );
+}
