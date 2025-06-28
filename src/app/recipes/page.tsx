f35deb392127a0f69c6564138bda0ff6bf92e079
rknm-cell <racknahm@gmail.com>
2025-06-28 14:42:26 -0400

    added searchbar

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,22 +1,31 @@
 "use client";
 import { api } from "~/trpc/react";
 import { RecipeCard } from "../components/recipes/RecipeCard";
+import SearchBar from "../components/SearchBar";
+import { useState } from "react";
 
 export default function Page() {
   const { data: recipes } = api.recipe.getAllRecipes.useQuery();
-
-  const recipeList = recipes ?? [];
+  const [search, setSearch] = useState("");
+  const recipeList = (recipes ?? []).filter((recipe) =>
+    recipe.name.toLowerCase().includes(search.toLowerCase()),
+  );
 
   return (
-    <div className="mx-auto p-4 bg-gradient-to-b from-[#1d7b86] to-[#426b70]">
-      <h1 className="text-3xl font-bold mb-6 text-center">Recipes {recipeList.length}</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+    <div className="mx-auto bg-gradient-to-b from-[#1d7b86] to-[#426b70] p-4">
+      <h1 className="mb-6 text-center text-3xl font-bold">
+        Recipes {recipeList.length}
+      </h1>
+      <SearchBar search={search} setSearch={setSearch} />
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {recipeList.length > 0 ? (
           recipeList.map((recipe, index) => (
             <RecipeCard key={recipe.id || index} recipe={recipe} />
           ))
         ) : (
-          <p className="col-span-full text-center text-gray-500">No recipes found</p>
+          <p className="col-span-full text-center text-gray-500">
+            No recipes found
+          </p>
         )}
       </div>
     </div>
